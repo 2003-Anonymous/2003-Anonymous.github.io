695c7109d02c2ce7669645d3bba47919348ae918
2003-Anonymous <J.Sommerhalder.inf24@stud.bbbaden.ch>
2026-05-22 11:39:19 +0200

    New Website

--- script.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.getElementById("sidebar");
-const toggleBtn = document.getElementById("toggleBtn");
-
-toggleBtn.onclick = function () {
-   sidebar.classList.toggle("expanded");
-};
